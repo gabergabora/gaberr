25f6f5ecc3d5b64a1e798952fdc70b534732c2a4
gabergabora <102705568+gabergabora@users.noreply.github.com>
2023-03-28 21:14:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const path = require('path');
 
 const errorController = require('./controllers/error');
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 
+app.all("*",  (req, res, next) => {
+  res.render("404", { title: "Page Not Found" });
+});
 
 
 app.get('/', (req, res) => {
