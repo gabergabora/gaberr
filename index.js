6f8d4ba6491231faae5b4e957fcad4ecbba08c39
gabergabora <102705568+gabergabora@users.noreply.github.com>
2023-03-28 21:13:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const errorController = require('./controllers/error');
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+
+
+app.all("*", authController.protect, (req, res, next) => {
+  res.render("404", { title: "Page Not Found" });
+});
+
 app.get('/', (req, res) => {
     res.send('Home Page Route');
 });
