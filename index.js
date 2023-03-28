5c90436c11e0380842dd41ab5611fa940d1eda28
gabergabora <102705568+gabergabora@users.noreply.github.com>
2023-03-28 21:30:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const bodyParser = require("body-parser")
 const cookieParser = require("cookie-parser")
 const methodOverride = require("method-override");
+const rateLimit = require('express-rate-limit');
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -16,7 +17,85 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(methodOverride('_method'))
 
+const contactLimiter = rateLimit({
+    windowMs: 60000,
+    max: 1,
+    message: 'To discourage spam, there is a message cooldown of 1 minute.',
+    skipFailedRequests: true,
+    requestWasSuccessful: function (req, res) {
+        const { contactStatus } = req.signedCookies;
+        return contactStatus !== 'error' || contactStatus !== 'invalid';
+    },
+    handler: function (req, res) {
+        res.cookie('rateLimited', 'true', { signed: true, secure: true, httpOnly: true, maxAge: 60000 });
+        res.redirect('/contact');
+    }
+});
 
+function getIP(req) {
+    return req.headers['x-real-ip'] || req.socket.remoteAddress;
+}
+
+app.post('/contact', contactLimiter, async function (req, res) {
+    try {
+        if (!format.check(req.body)) {
+            res.cookie('contactStatus', 'invalid', { signed: true, secure: true, httpOnly: true, maxAge: 60000 });
+            return res.redirect('/contact');
+        }
+
+        for (let key in req.body) {
+            req.body[key] = format.sanitize(req.body[key]);
+        }
+
+        const { name, email, body, token } = req.body;
+
+        const recaptcha = await axios.post('https://www.google.com/recaptcha/api/siteverify', `secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}&remoteip=${getIP(req)}`, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+
+        const success = recaptcha.data.success;
+        const score = recaptcha.data.score;
+
+        if (!success || score < 0.5) {
+            res.cookie('contactStatus', 'recaptchaFailed', { signed: true, secure: true, httpOnly: true, maxAge: 60000 });
+            return res.redirect('/contact');
+        }
+
+        let newMessage = new Message({ name: name, email: email, body: body, meta: { ip: getIP(req), date: new Date(), recaptcha: { success: success, score: score } } });
+        await newMessage.save();
+
+        notify.message(name, email, body, getIP(req));
+        res.cookie('contactStatus', 'success', { signed: true, secure: true, httpOnly: true, maxAge: 60000 });
+        res.redirect('/contact');
+    } catch (err) {
+        console.log(err);
+        notify.alert(err, getIP(req)).catch();
+        res.cookie('contactStatus', 'error', { signed: true, secure: true, httpOnly: true, maxAge: 60000 });
+        res.redirect('/contact');
+    }
+});
+
+app.get('/contact', function (req, res) {
+    const { contactStatus = null, rateLimited = false } = req.signedCookies;
+
+    if (!contactStatus && !rateLimited) {
+        res.status(400);
+        res.redirect('/');
+    } else {
+        if (rateLimited === 'true') {
+            res.clearCookie('rateLimited', { signed: true, secure: true, httpOnly: true });
+            res.status(429);
+            res.render('status', { status: '429', title: 'Error' });
+        } else {
+            if (contactStatus === 'error') {
+                res.status(500);
+            }
+            res.render('status', { status: contactStatus, title: 'Contact' });
+        }
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('Home Page Route');
