fdfb6572ef40f71560d9310d2f4781e3a524d852
gabergabora <102705568+gabergabora@users.noreply.github.com>
2023-03-28 21:25:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const path = require('path');
-
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, 'assets')))
 
 
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(methodOverride('_method'))
 
 
 
